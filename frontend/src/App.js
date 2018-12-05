596b607e33672e4983980ed8c80024cdb555096b
MarkCBall <moball@hotmail.com>
2018-12-05 15:20:17 -0500

    initialize react frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+
+        <p>Hi</p>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
