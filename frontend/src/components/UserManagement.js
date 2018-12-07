2268876ad2c663eefb957116cc32749043885b85
MarkCBall <moball@hotmail.com>
2018-12-07 08:51:24 -0500

    issues fixed length vs unbounded arrays I think

--- frontend/src/components/UserManagement.js
+++ frontend/src/components/UserManagement.js
@@ -12,9 +12,15 @@ class UserManagement extends Component {
         this.state ={
             owner:"Not Updated",
             numAccts: -1,
-            contractBal:-1
+            contractBal:-1,
+            accounts:[
+                {own:"addy1",balv1:2,balv2:2.1},
+                {own:"addy2",balv1:2,balv2:2.1}
+            ]
         }
 
+        console.log(this.state.accounts[0].own)
+        console.log(this.state.accounts)
     this.GetData();
     }
     //this.state.r = "yyy"
@@ -30,18 +36,29 @@ class UserManagement extends Component {
         MyContract.numAccts.call( (e,response) => {
             this.setState({numAccts:response.c[0]})//FIX THIS WEIRDNESS!
             //AccountsArray
-            let arr;//put the console logs into an array but figure out async ordering first
+            let arr = [];
             for (let i=0;i<response.c[0];i++){
-                MyContract.Accounts(//THIS IS ASYNC - HOW TO MAKE SURE IT GOES IN ORDER???
+                MyContract.Accounts(
                     i,
-                    (e,response) => {
-                        console.log("account# "+i)
-                        console.log("The owner is " +response[0])
-                        console.log("double check bal is " +response[1].c[0])
-                        console.log("or is the bal "+ response[1].s)
+                    (e,res) => {
+                        let ownr = res[0];
+                        let balv1r = res[1].c[0];
+                        let balv2r = res[1].s;
+                        arr[i]={own:"addy1xx",balv1:20,balv2:20.1};//{own:ownr,balv1:balv1r,balv2:balv2r};
+                        
+                        // console.log("accXXcount# "+i)
+                        // console.log(i+"The owner is " +response[0])
+                        // console.log(i+"double check bal is " +response[1].c[0])
+                        // console.log(i+"or is the bal "+ response[1].s)
                     }
                 )
             }
+            
+            //arr[0]={own:"addy1xx",balv1:20,balv2:20.1};
+            //arr[1]={own:"addy2xx",balv1:20,balv2:20.1};
+            console.log(arr);
+            console.log(arr[0])
+            //this.setState({accounts:arr})
 
         });
         
@@ -62,6 +79,11 @@ class UserManagement extends Component {
             <p>The owner of the account is: <strong>{this.state.owner}</strong></p>
             <p>The smart contract holds <strong>{this.state.numAccts}</strong> accounts</p>
             <p>The smart contract holds <strong>{this.state.contractBal/1000000000000000000}</strong> eth</p>
+            
+            <p>
+            {/* {this.state.accounts[0].own} */}
+            </p>
+            {/* <p>{this.state.accounts[1]}</p> */}
 
 
 
