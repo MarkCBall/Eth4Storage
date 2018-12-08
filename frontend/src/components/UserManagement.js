e757bc02bd06b7c0d3ea194646f002b65022f8ac
MarkCBall <moball@hotmail.com>
2018-12-07 22:38:35 -0500

    BiGNumber

--- frontend/src/components/UserManagement.js
+++ frontend/src/components/UserManagement.js
@@ -16,7 +16,6 @@ class UserManagement extends Component {
             contractBal:-1,
             accounts:[
                 {key:0, own:"addy1", balv1:2, balv2:2.1},
-                {key:1, own:"addy2", balv1:2, balv2:2.1}
             ]
         }
     this.GetData();
@@ -34,44 +33,36 @@ class UserManagement extends Component {
 
         //numAccounts
         MyContract.numAccts.call( (e,response) => {
-            this.setState({numAccts:response.c[0]})//FIX THIS WEIRDNESS!
-            //AccountsArray - done inside callback
-            let arr = []
+            this.setState({numAccts:response.c.toString(10)})
+            //AccountsArray
+            let arr = [];
             for (let i=0;i<response.c[0];i++){
                 MyContract.Accounts(
                     i,
                     (e,res) => {
-                        let ownr = res[0];
-                        //im not sure which one is the correct balance and can't check now as all bals are exactly 1
-                        let b1 = res[1].c[0];
-                        let b2 = res[1].s;
-                        arr.push({key:i,own:ownr,balv1:b1,balv2:b2});
-                        this.setState({accounts:arr})
+                        arr.push({
+                            key:i,
+                            own:res[0],
+                            bal:res[1].toString(10)
+                        });
+                        this.setState({accounts:arr});
                     }
                 )
             }
         });
-        
         //UsersArray?? need to provide specific addy? diff for each Acct?
-
-        
-
     }
 
     render() {
         return (
             <div className="main-tile tabbed">
-
-                <br></br><br></br><br></br><br></br><br></br>
                 <h1>This is the UserManagement page</h1>
-                <p>Session ID:{this.props.sessionID}</p>
-
-                <p>The owner of the contract is: <strong>{this.state.owner}</strong></p>
                 <p>
-                    The smart contract holds <strong>{this.state.numAccts}</strong> accounts and 
-                    <strong>{this.state.contractBal/1000000000000000000}</strong> eth
+                    The contract address is: <strong>{ContractAddress}</strong> and it has
+                    <strong> {this.state.numAccts}</strong> accounts and 
+                    <strong> {this.state.contractBal/1000000000000000000}</strong> eth
                 </p>
-                <hr></hr><br></br>
+                <hr></hr>
                 
                 
                 {this.state.accounts.map(
@@ -79,7 +70,7 @@ class UserManagement extends Component {
                         <div className="Accounts" key={acct.key}>
                             <strong>AccountID: </strong>{acct.key}<br></br> 
                             <strong>Owned by: </strong>{acct.own}<br></br> 
-                            <strong>with balance of: </strong>{acct.balv1} or {acct.balv2}<br></br>
+                            <strong>with balance of: </strong>{acct.bal} <br></br>
                             <div className="tabbed">
                                 <strong>Address Permissions:</strong><br></br>
                                 {/* how to let the user add stuff here?? */}
