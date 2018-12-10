c6a39e6e41948ce08de8886b265afc29872fe4fc
dylanbaker21 <dylan.j.baker@hotmail.com>
2018-12-10 13:49:19 -0500

    owner can change account price + error message support

--- solidity/AccountMngmt.sol
+++ solidity/AccountMngmt.sol
@@ -1,13 +1,17 @@
-//deployed to 0x7023aCfD191cf652db0168d67bF6652f9e4aBA72 ropstein - deployed from mark's good computer linux 
+//deployed to 0x4a36137D3423737Cde187B7aF074291302707fDd ropsten - deployed from dylan's windows desktop
 
 pragma solidity ^0.4.25;
-
+    
 contract AccountMngmt {
+    
     address public owner;
     uint public numAccts=0;
+    uint public currentAccPrice;
+    
     //map of accounts - owning address + eth balance
     struct Admin {address AdminAddr; uint Bal;}
     mapping (uint => Admin) public Accounts;
+    
     //map of users - associated account + write access --> all users have view permission
     struct UserStruct {uint AcctId; bool CanWrite;}
     mapping (address => UserStruct) public Users;
@@ -16,63 +20,66 @@ contract AccountMngmt {
     
     constructor() public {
         owner = msg.sender;
+        currentAccPrice = 100000000000000000; // unit = wei (0.1 ether)
+    }
+    function setAccPrice(uint _newPrice) public {
+        require(msg.sender == owner, "You must be the owner to change the price.");
+        currentAccPrice = _newPrice * 1 wei;
     }
     function giveOwnership(uint _Acct, address _newowner) public {
         //ensure the message sender is the admin of the account
-        require(Accounts[_Acct].AdminAddr == msg.sender);
-        //change the account's admin
+        require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to transfer ownership");
+        //change the account’s admin
         Accounts[_Acct].AdminAddr = _newowner;
     }
     function approveViewer(uint _Acct, address _User) public {
         //ensure the message sender is the admin of the account
-        require(Accounts[_Acct].AdminAddr == msg.sender);
+        require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to approve viewers");
         //add the user linked to the account
         Users[_User].AcctId = _Acct;
     }
     function approveWriter(uint _Acct, address _User) public {
         //ensure the message sender is the admin of the account
-        require(Accounts[_Acct].AdminAddr == msg.sender);
+        require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to approve writers");
         //add the user linked to the account and give write permission
         Users[_User].AcctId = _Acct;
         Users[_User].CanWrite = true;
     }
     function deleteUser(uint _Acct, address _User) public {
         //ensure the message sender is the admin of the account
-        require(Accounts[_Acct].AdminAddr == msg.sender);
+        require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to delete users");
         //delete the user
         delete Users[_User];
     }
     function disallowWrite(uint _Acct, address _User) public {
         //ensure the message sender is the admin of the account
-        require(Accounts[_Acct].AdminAddr == msg.sender);
-        //remove the user's write access
+        require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to disallow writers");
+        //remove the user’s write access
         Users[_User].CanWrite = false;
     }
     function ownerWithdraw(uint _Amount) public{
-        require(msg.sender == owner);
+        require(msg.sender == owner, "You must be the contract owner to withdraw");
         //msg.sender.send(_Amount);
         msg.sender.transfer(_Amount);
         //require(msg.sender.send(_Amount));
     }
     //create an account only with a specific function call to minimize mistakes
     function createAccount() public payable returns(uint){
-        //minimum price to hold an account is 1 eth
-        require(msg.value > 1 ether); // 1 or 1 000 000 or 1eth?
+        //minimum price to hold an account is 0.1 eth
+        require(msg.value >= currentAccPrice * 1 wei, "You must send enough funds to create an account");
         //set up account
         Accounts[numAccts].AdminAddr = msg.sender;
-        Accounts[numAccts].Bal = 1;
+        Accounts[numAccts].Bal = currentAccPrice * 1 wei;
         //if extra eth was sent, return it
-        if (msg.value > 1 ether){
-            msg.sender.transfer(msg.value-1 ether);  
+        if (msg.value > currentAccPrice * 1 wei){
+            msg.sender.transfer(msg.value - currentAccPrice * 1 wei);
         }
         //counter for current account number
         numAccts++;
         return (numAccts-1);
-        //combine into return(numAccts++) as ++ done after return? 
+        //combine into return(numAccts++) as ++ done after return?
     }
-    function() payable external{
-        //can this be done better?
-        revert();
+    function() external payable {
+        revert("You must call a function to interact with this contract");
     }
-    
 }
