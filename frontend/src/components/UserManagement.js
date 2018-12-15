3b155dd9669865fa310af4e24b84183a717b66c0
MarkCBall <moball@hotmail.com>
2018-12-14 22:49:44 -0500

    restructured code

--- frontend/src/components/UserManagement.js
+++ frontend/src/components/UserManagement.js
@@ -1,76 +1,77 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
+//relative imports redux items
 import { addAccount } from "../redux/actions/todo";
 import { addUserToAccount } from "../redux/actions/todo";
 
+//relative imports react items
 import TitleTile from './SubUserManagement/TitleTile'
 import RenderRow from './SubUserManagement/RenderRow'
 import RenderSubRow from './SubUserManagement/RenderSubRow'
 import HeaderRow from './SubUserManagement/HeaderRow'
 import FooterSubRow from './SubUserManagement/FooterSubRow'
 
+//relative imports smart contract data
 import ContractABI, {ContractAddress} from '../ContractABI';
 
 //CSS Files
 import './SubUserManagement/UserManagement.css'
-//import { connect } from 'http2';
+
 
 class UserManagement extends Component {
     constructor(props){
         super(props)
         this.state ={
-            contract: "contract not queried yet",
-            numAccts: -1,
-            contractBal:-1,
             isExpanded:[]
-
         }
-        var MyContract = this.GetContract();
-        this.contractToState(MyContract);
+
+
+////////////////START GLOBAL STATE STUFF TO PUT ELSEWHERE
+        this.contractToState();
     }
 
-    GetContract() {
-        return window.web3.eth.contract(ContractABI).at(ContractAddress);
-    }
-
-    contractToState(Contract) {
+    contractToState() {
+        //get an instance of the smart contract
+        let Contract = window.web3.eth.contract(ContractABI).at(ContractAddress);
+        //find the number of accounts
         Contract.accountCount.call( (e,response) => {
             let numAccts = parseInt(response.c.toString(10));
-            this.setState({numAccts:numAccts})
+            //loop through the accounts
             for (let acctNum=0;acctNum<numAccts;acctNum++){
+                //get the account data (owner's address and balance)
                 Contract.Accounts(acctNum,(e,res) => {
+                    //add each account to global state
                     this.setAccountState(acctNum,e,res)
-
-
-                    //var Contract = window.web3.eth.contract(ContractABI).at(ContractAddress);
-                    //var acctNum = this.props.acctNum;
-                    //find the # of users and loop through them
+                    //get the number of users for the account
                     Contract.userCountsInAccount.call(acctNum, (e,response) => {
                         let numUsers = response.c.toString(10);
+                        //loop through each user
                         for (let i=0;i<numUsers;i++){
-                            //add each user to global state
+                            //get the user's data (address, and write permission)
                             Contract.usersOfAccount(acctNum,i,(e,r)=>{
+                                //add each user to global state
                                 this.addUser(acctNum,i,e,r)
                             })
                         }
                     })
-
                 }) 
             }
-            //when should state be sorted??
         })
     }
-
-     //changes global state to add user to account
-     addUser(acctNum,i,e,r){
+    //changes global state to add user to account
+    addUser(acctNum,i,e,r){
         this.props.addUserToAccount(acctNum,  {key:i, addy:r[0], canWrite : r[1]}    );
     }
     //changes global state to add account
     setAccountState = (i,e,r) => {
         this.props.addAccount({key:i,own:r[0],bal:r[1].toString(10)})
     }
+////////////////END GLOBAL STATE THATS ABOVE//////////////////
 
+
+
+    
     //changes the status of is users are displayed under the account
     ToggleUsers (acctNum) {
         let tmparr = this.state.isExpanded;
@@ -90,7 +91,7 @@ class UserManagement extends Component {
                 <TitleTile title="User Management Page">
                     <p>
                         The contract address is: <strong>{ContractAddress}</strong> and it has
-                        <strong> {this.state.numAccts}</strong> account(s)
+                        <strong> {this.props.state.todo.accounts.length}</strong> account(s)
                     </p>
                 </TitleTile>
                 <div className="container-full">
@@ -140,15 +141,11 @@ class UserManagement extends Component {
     };
 };
 
-//export default UserManagement;
 const mapStateToProps = function(state){
     return{
         state
-        //xxx:state.test.foo,
-        //yyy:state.todo.todos
     }
 }
-
 
 export default connect(mapStateToProps,{addAccount,addUserToAccount})(UserManagement)
 
