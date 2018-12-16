8ae5e741611d1fb3d75a5957ec3995af4bba78ac
MarkCBall <moball@hotmail.com>
2018-12-16 11:24:56 -0500

    query contract fully syncronously -semi sync would be better?

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 
 //relative imports redux items
@@ -7,7 +7,7 @@ import { addAccount } from "../redux/actions/todo";
 import { addUserToAccount } from "../redux/actions/todo";
 
 //relative imports smart contract data
-import ContractABI, {ContractAddress} from '../ContractABI';
+import ContractABI, { ContractAddress } from '../ContractABI';
 
 //CSS Files
 import './Header.css';
@@ -15,58 +15,51 @@ import './Header.css';
 
 
 class Header extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.contractToState();
     }
 
+  // //WHERE WOULD THE GLOBAL STATE IDEALLY BE SET?????????
+    // //its done here in the header since the header only loads once... better ideas?
 
 
-    //WHERE WOULD THE GLOBAL STATE IDEALLY BE SET?????????
-    //its done here in the header since the header only loads once... better ideas?
-    contractToState() {
-        //possible better algorithm, to allow sorting?
-        //AWAIT get # of accounts
-        //AWAIT loop through all accounts and get data
-        //sort accounts order
-        //AWAIT loop through accounts and get # of users in each account
-        //AWAIT loop through each user and get data
-        //loop through accounts and sort users
+    promisify = (inner) =>
+        new Promise((resolve, reject) =>
+            inner((err, res) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(res);
+                }
+            })
+        )
 
-        //get an instance of the smart contract
+    async contractToState() {
+        //how could this be done SEMI SYNCRONOUSLY?
+            //ie only num accounts and num users need to be awaited
+            //and numusers only needs to be awaited INSIDE the account's scope
+
+        //initialize
         let Contract = window.web3.eth.contract(ContractABI).at(ContractAddress);
         //find the number of accounts
-        Contract.accountCount.call( (e,response) => {
-            let numAccts = parseInt(response.c.toString(10));
-            //loop through the accounts
-            for (let acctNum=0;acctNum<numAccts;acctNum++){
-                //get the account data (owner's address and balance)
-                Contract.Accounts(acctNum,(e,res) => {
-                    //add each account to global state
-                    this.setAccountState(acctNum,e,res)
-                    //get the number of users for the account
-                    Contract.userCountsInAccount.call(acctNum, (e,response) => {
-                        let numUsers = response.c.toString(10);
-                        //loop through each user
-                        for (let i=0;i<numUsers;i++){
-                            //get the user's data (address, and write permission)
-                            Contract.usersOfAccount(acctNum,i,(e,r)=>{
-                                //add each user to global state
-                                this.addUser(acctNum,i,e,r)
-                            })
-                        }
-                    })
-                }) 
+        let resNumAccts = await this.promisify( cb => Contract.accountCount.call(cb) )
+        let numAccts = parseInt(resNumAccts.toString(10));
+        //loop through the accounts
+        for (let acctNum = 0; acctNum < numAccts; acctNum++) {
+            let resAcct = await this.promisify( cb => Contract.Accounts(acctNum,cb) )
+            //call dispatch to add account to store
+            this.props.addAccount({ key: acctNum, own: resAcct[0], bal: resAcct[1].toString(10) })   
+            //get the number of users for the account
+            let resNumUsers = await this.promisify( cb => Contract.userCountsInAccount.call(acctNum,cb) )
+            let numUsers = resNumUsers.toString(10);
+            //loop through each user
+            for (let userNum = 0; userNum < numUsers; userNum++) {
+                let resUser = await this.promisify( cb => Contract.usersOfAccount(acctNum,userNum,cb) )
+                //call dispatch to add account to store
+                this.props.addUserToAccount(acctNum, { key: userNum, addy: resUser[0], canWrite: resUser[1] });
             }
-        })
-    }
-    //changes global state to add user to account
-    addUser(acctNum,i,e,r){
-        this.props.addUserToAccount(acctNum,  {key:i, addy:r[0], canWrite : r[1]}    );
-    }
-    //changes global state to add account
-    setAccountState = (i,e,r) => {
-        this.props.addAccount({key:i,own:r[0],bal:r[1].toString(10)})
+        }     
     }
 
     render() {
@@ -75,16 +68,16 @@ class Header extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-sm">
-                        <br></br>
-                        <Link to="Upload">Upload Page</Link>
+                            <br></br>
+                            <Link to="Upload">Upload Page</Link>
                         </div>
                         <div className="col-sm">
-                        <br></br>
-                        <Link to="Download">Download Page</Link>
+                            <br></br>
+                            <Link to="Download">Download Page</Link>
                         </div>
                         <div className="col-sm">
-                        <br></br>
-                        <Link to="UserManagement">User Management Page</Link>
+                            <br></br>
+                            <Link to="UserManagement">User Management Page</Link>
                         </div>
                     </div>
                 </div>
@@ -93,6 +86,12 @@ class Header extends Component {
     };
 };
 
-export default connect(null,{addAccount,addUserToAccount})(Header)
+const mapStateToProps = function (state) {
+    return {
+        state
+    }
+}
+
+export default connect(mapStateToProps, { addAccount, addUserToAccount })(Header)
 
 
