5772f85181303e15ad5f160f174a17131f06a96c
MarkCBall <moball@hotmail.com>
2018-12-16 11:24:05 -0500

    better download state structure

--- frontend/src/components/Download.js
+++ frontend/src/components/Download.js
@@ -1,66 +1,61 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 
 class Download extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          rArr:{},
-          wArr:{}
+            canWriteObj: {}
         }
-        //this loads before global state is set sometimes
-        //how could we force this to only happen if global state is finished loading?
-      }
+        //why can't this be here?
+        //this.logAccountsForAddress(this.props.verifiedAddress)
+    }
 
-      componentDidMount(){
-
-        //this is confusing me, if I put this into the constructor I get the error:
-            //Warning: Can't call setState on a component that is not yet mounted. This is a no-op, but it might indicate a bug in your application. Instead, assign to `this.state` directly or define a `state = {};` class property with the desired state in the Download component.
-            //WHY?????
-        //because of this 'fix' you need to navigate away and navigate back to this page to update...
+    componentDidMount() {
+        //console.log("mounting")
         this.logAccountsForAddress(this.props.verifiedAddress)
-      }
+    }
 
-    addElemToPermissionsArr(elem){
-        //i'm sure this can be done better but leaving it for now
+    componentWillUpdate() {
+        //console.log("updating")
+        //this.logAccountsForAddress(this.props.verifiedAddress)
+    }
 
+    addElemToPermissionsArr(elem) {
+        //console.log("elem added is", elem)
         this.setState(prevState => ({
-            rArr: { ...prevState.rArr , [elem.acctN]:true  }
-        }))
-
-        this.setState(prevState => ({
-            wArr: { ...prevState.rArr , [elem.acctN]:elem.canWrite  }
+            canWriteObj: { ...prevState.canWriteObj, [elem.acctN]: elem.canWrite }
         }))
     }
-    
+
 
 
     //returns an array of users for a given account number
     getUserArray(acctN) {
         let accounts = this.props.state.todo.accounts;
         let arrIndex = accounts.findIndex(o => o.key === acctN);
-        if (arrIndex>=0 && accounts[arrIndex].users)
+        if (arrIndex >= 0 && accounts[arrIndex].users)
             return (accounts[arrIndex].users)
         return []
 
     }
     //returns an array of strings with the permissions the logged in metamask's account has
-    logAccountsForAddress(address){
-    
+    logAccountsForAddress(address) {
+
         let accts = this.props.state.todo.accounts;
         //loop through all accounts
-        for (let i=0;i<accts.length;i++){
+        for (let i = 0; i < accts.length; i++) {
             //loop through the users of the specific account
             let userArr = this.getUserArray(accts[i].key);
-            for (let j=0;j<userArr.length;j++){
+            for (let j = 0; j < userArr.length; j++) {
                 //check if the logged in account has permission as a user
                 if (userArr[j].addy === address)
-                    if (userArr[j].canWrite){
-                        this.addElemToPermissionsArr({acctN:accts[i].key,canWrite:true})
+                    if (userArr[j].canWrite) {
+                        this.addElemToPermissionsArr({ acctN: accts[i].key, canWrite: true })
                     }
-                    else{
-                        this.addElemToPermissionsArr({acctN:accts[i].key,canWrite:false})
+                    else {
+                        this.addElemToPermissionsArr({ acctN: accts[i].key, canWrite: false })
                     }
             }
         }
@@ -69,9 +64,12 @@ class Download extends Component {
     render() {
         return (
             <div className="main-tile">
+
+
+
                 <h1>This is the download page</h1>
                 <p>Login to see what you have access to</p>
-                
+
                 <p>In future you can click on an account number and be taken to relevant upload/download page of that account</p>
                 <p>NOTE!!! --- YOU MUST NAVIGATE TO THIS PAGE AFTER LOGGING IN TO RENDER ANYTHING</p>
                 <p>(THIS PAGE IS IN DEVELOPMENT)</p>
@@ -88,28 +86,26 @@ class Download extends Component {
                             CanWrite
                         </div>
                     </div>
-                
-                    {Object.keys(this.state.rArr).map( (x)=>(
+
+                    {Object.keys(this.state.canWriteObj).map((x) => (
                         <div className="row" key={x}>
                             <div className="col-1">
                                 {x}
                             </div>
                             <div className="col-1">
-                                {this.state.rArr[x]?"yes":"no"}
+                                yes
                             </div>
-                             <div className="col-1">
-                             {this.state.wArr[x]?"yes":"no"}
+                            <div className="col-1">
+                                {this.state.canWriteObj[x] ? "yes" : "no"}
                             </div>
-                        </div>      
-                    ))}       
-    
+                        </div>
+                    ))}
+
                 </div>
-     
-                <button onClick={()=> console.log(
-                    this.state.readArr,
-                    this.state.writeArr,
-                    this.state.rArr,
-                    this.state.wArr                
+
+
+                <button onClick={() => console.log(
+                    this.state.canWriteObj
                 )}>Console.log permissions for debugging</button>
 
             </div>
@@ -117,9 +113,12 @@ class Download extends Component {
     };
 };
 
-const mapStateToProps = function(state){
-    return{
+const mapStateToProps = function (state) {
+    return {
         state
     }
 }
 export default connect(mapStateToProps)(Download)
+
+
+//<p>{this.props.state.todo.accounts[0].key}</p>
