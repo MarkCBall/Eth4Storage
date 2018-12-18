7ad73f87b1382671b06f7197823bcf73fd16e415
MarkCBall <moball@hotmail.com>
2018-12-18 02:20:46 -0500

    cors fixed - sending data in header and back in response

--- frontend/src/components/Download.js
+++ frontend/src/components/Download.js
@@ -19,9 +19,42 @@ class Download extends Component {
         )
     }
 
-    handleSelection(acctN){
-        this.setState({selectedAcct:acctN})
+    handleSelection(acctN) {
+        this.setState({ selectedAcct: acctN })
     }
+
+
+    queryServer() {
+        //get req to server
+        fetch('http://localhost:3000/users',{
+            method:'GET', // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            //credentials: "same-origin", // include, *same-origin, omit
+            headers: {data:"hi"}
+                // "Content-Type": "application/json; charset=utf-8",
+            //     // "Content-Type": "application/x-www-form-urlencoded",
+     
+
+
+            // redirect: "follow", // manual, *follow, error
+            // referrer: "no-referrer", // no-referrer, *client
+
+        }).then(function(response) {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data ? JSON.parse(data) : {}  )
+            
+            //this.setState({accounts:JSON.parse(data)})
+            // console.log("state is set to")
+            // console.log(data);
+        })
+    }
+
+
+
+
 
     render() {
         return (
@@ -29,19 +62,20 @@ class Download extends Component {
                 <h1>This is the download page</h1>
                 <nav className="navbar navbar-expand-lg">
                     {Object.keys(this.getPermissions()).map((acct) => (
-                        <button key={acct} onClick={()=>this.handleSelection(acct)} className="navbar-brand" >
+                        <button key={acct} onClick={() => this.handleSelection(acct)} className="navbar-brand" >
                             Acnt#{acct}
                         </button>
                     ))}
                 </nav>
 
                 <div className="card">
-                <div className="card-body">
-                    <h4 className="card-title">Account# {this.state.selectedAcct} download page</h4>
-                    <p className="card-text">View existing files you can download </p>
-                </div>
+                    <div className="card-body">
+                        <h4 className="card-title">Account# {this.state.selectedAcct} download page</h4>
+                        <p className="card-text">View existing files you can download </p>
+                    </div>
                 </div>
 
+                <button onClick={()=> this.queryServer()}>Query Server</button>
 
             </div>
         );
