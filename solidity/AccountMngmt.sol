af338ce6a45164ea9062f664e4206e87f8ed33f3
MarkCBall <moball@hotmail.com>
2019-01-18 16:32:17 -0500

    new contract framework

--- solidity/AccountMngmt.sol
+++ solidity/AccountMngmt.sol
@@ -1,27 +1,32 @@
-//deployed to 0x14A0038Cb38C5e530c85995A335F16a1A4817B02 ropsten - deployed from dylan's mac
-
 pragma solidity ^0.4.25;
     
 contract AccountMngmt {
     
-    address public owner;
-    uint public accPrice;
-    uint public userPrice;
-    uint public initialBal;
-
+    //STATE ERC20 based 
+    // string public symbol;// string public  name;// uint8 public decimals;
+    uint totalSupply;
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+    //STATE user management
     Account[] public Accounts;
     struct Account {address AdminAddr; uint Bal; User[] Users;}
     struct User {address UserAddy; bool CanWrite;}
+    uint public accPrice;//price to make an account
+    uint public userPrice;//price to add a user
     
+    //ERC20 events
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+    
+    //constructor function
     constructor() public {
         owner = msg.sender;
         // currency unit is wei
         accPrice = 500000000000000000;
         userPrice = 100000000000000000;
-        initialBal = 325000000000000000;
     }
 
-    // Owner functions
+    //SETTER FUNCTIONS
     function setAccPrice(uint _newAccPrice) public {
         require(msg.sender == owner, "You must be the owner to change the account price.");
         accPrice = _newAccPrice * 1 wei;
@@ -34,30 +39,64 @@ contract AccountMngmt {
         require(msg.sender == owner, "You must be the owner to change the initial balance.");
         initialBal = _newInitialBal * 1 wei;
     }
-    function ownerWithdraw(uint _Amount) public{
-        require(msg.sender == owner, "You must be the contract owner to withdraw");
-        msg.sender.transfer(_Amount);
+
+    //ERC20 functions - need to complete
+    function totalSupply() public view returns (uint){
+        return totalSupply;
+    }
+    function balanceOf(address tokenOwner) public view returns (uint balance){
+        return balances(tokenOwner);
+    }
+    function allowance(address tokenOwner, address spender) public view returns (uint remaining){
+        return allowed(tokenOwner(spender));
+    }
+    function transfer(address to, uint tokens) public returns (bool success){
+        
+    }
+    function approve(address spender, uint tokens) public returns (bool success){
+        
+    }
+    function transferFrom(address from, address to, uint tokens) public returns (bool success){
+        
     }
 
-    // Core functions
-    function createAccount() public payable {
-        //ensure total price is paid
-        require(msg.value >= accPrice * 1 wei, "You must send enough funds to create an account");
-        //set up account
-        Accounts.length++;
-        uint acctN = Accounts.length-1;
-        Accounts[acctN].AdminAddr = msg.sender;
-        Accounts[acctN].Bal = initialBal;
+    //bonding curve functions - need to do math to figure out #tokens given #eth -- // need to add events to buy and sell tokens
+    function priceAdjusted(uint s, int b){
+    //let p = Price of tokens in eth/token
+    //let s = Total current supply of tokens
+    //let e = Total ethereum in the smart contract
+    //let b = the number of tokens to buy
+    //the price for a token will be the total supply^2
+    //p=s^2
+    //The integral of p=s^2 is e= ⅓ supply^3
+    //The number of eth needed to buy b tokens is given by:
+    // (1 / 3)(s+b)^3 - (1 / 3)(s)^3
+    //refactored to s^2+b^2 /3+sb
+    //The average price to buy b tokens is number of eth needed divided by b
+    //NOTE THIS FAILS DUE TO ROUNDING ERRORS AND FRONT RUNNING CONCERNS
+    return (s*b +   s*s + (b*b/3))/b;
     }
-    function addFunds(uint _Acct) public payable {
-        require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to add funds");
-        Accounts[_Acct].Bal += msg.value;
+    function buyTokens(uint numTokens) public payable{
+        uint equivEth = priceAdjusted(totalSupply,numTokens)*numTokens;
+        assert (msg.val >= equivEth)
+        balances(msg.sender)+=numTokens;
+        totalSupply+=numTokens;
     }
+    function sellTokens(uint numTokens) public{
+        uint equivEth = priceAdjusted(totalSupply,numTokens)*numTokens;
+        assert(balances(msg.sender)>=numTokens);
+        balances(msg.sender) -= numTokens;
+        totalSupply -= numTokens;
+        msg.sender.send(equivEth);
+        //make it so eth isnt withdrawn instantly to prevent someone from using a service and withdrawing instantly. Delay by 100, 1000 blocks?//create a mapping (address -> WithdrawableEth)//struct WithdrawableEth {timestamp, amount of eth}
+    }
+    
+    //user management functions - need to review
     function approveViewer(uint _Acct, address _User) public {
         //ensure message sender is admin of the account and has sufficient balance
         require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to approve viewers");
         require(Accounts[_Acct].Bal >= userPrice, "Not enough funds!");
-        Accounts[_Acct].Bal -= userPrice;
+        //fees not thought through
 
         //add the user linked to the account
         Accounts[_Acct].Users.length++;
@@ -68,7 +107,7 @@ contract AccountMngmt {
         //ensure message sender is admin of the account
         require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to approve viewers");
         require(Accounts[_Acct].Bal >= userPrice, "Not enough funds!");
-        Accounts[_Acct].Bal -= userPrice;
+        //fees not thought through
 
         //add user to the account
         Accounts[_Acct].Users.length++;
@@ -77,23 +116,19 @@ contract AccountMngmt {
         //give write permission
         Accounts[_Acct].Users[numUsersInAcct].CanWrite = true;
     }
-    function giveOwnership(uint _Acct, address _newowner) public {
-        //ensure message sender is admin of the account
-        require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to transfer ownership");
-        //change the account’s admin
-        Accounts[_Acct].AdminAddr = _newowner;
-    }
     function disallowWrite(uint _Acct, uint _UserNum) public {
         //ensure message sender is admin of the account
         require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to disallow writers");
         //remove user’s write access
         Accounts[_Acct].Users[_UserNum].CanWrite = false;
+        //fees not thought through
     }
     function allowWrite(uint _Acct, uint _UserNum) public {
         //ensure message sender is admin of the account
         require(Accounts[_Acct].AdminAddr == msg.sender, "You must be the account admin to allow writers");
         //give the user write access
         Accounts[_Acct].Users[_UserNum].CanWrite = true;
+        //fees not thought through
     }
     function deleteUser(uint _Acct, uint _UserNum) public {
         //ensure message sender is admin of the account
@@ -102,7 +137,7 @@ contract AccountMngmt {
         delete Accounts[_Acct].Users[_UserNum];
     }
 
-    // View functions
+    // View functions- do we need more
     function usersOfAccount(uint _Acct, uint _User) public view returns(address, bool){
         return (Accounts[_Acct].Users[_User].UserAddy,Accounts[_Acct].Users[_User].CanWrite );
     }
